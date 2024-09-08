fa71386070a9c5a6e406ea52cf591958d4ae36d6
Ayr2017 <ayrat.2013@ya.ru>
2024-09-08 12:59:04 +0300

    main: feat (models +)

---
+++ app/Models/Bolus.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bolus extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'number',
+        'version',
+        'batch_number',
+        'produced_at',
+    ];
+}
