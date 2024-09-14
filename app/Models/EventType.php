b70b08eba51d849ac09779d89da5df701bcfae03
Ayr2017 <ayrat.2013@ya.ru>
2024-09-14 15:22:45 +0300

    main: feat (events)

---
+++ app/Models/EventType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class EventType extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->slug = Str::upper(Str::slug($model->name,'_'));
+        });
+        static::updating(function ($model) {
+            $model->slug = Str::upper(Str::slug($model->name,'_'));
+        });
+    }
+
+    public function events():HasMany
+    {
+        return $this->hasMany(Event::class, 'event_type_id', 'id');
+    }
+}
