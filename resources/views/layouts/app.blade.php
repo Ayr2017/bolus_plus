bfee64c52e168c51897f7f5ea619f97f9ca077bd
nailgalimov <nailgalimov8@gmail.com>
2024-09-15 17:23:36 +0300

    BH-6:feat:sidebar changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,42 +15,42 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app" class="d-flex flex-column vh-100">
-        @include('layouts.partials.header')
+<div id="app" class="d-flex flex-column vh-100">
+    @include('layouts.partials.header')
 
-        <main class="flex-grow-1 d-flex flex-column">
-            <div class="container flex-grow-1">
-                @auth
-                    @include('layouts.partials.error-toast')
-                    @include('layouts.partials.success-toast')
+    <main class="flex-grow-1 d-flex flex-column">
+        <div class="container flex-grow-1">
+            @auth
+                @include('layouts.partials.error-toast')
+                @include('layouts.partials.success-toast')
 
-                    <div class="row h-100">
-                        <aside class="col-md-3 sidebar bg-secondary py-3">
-                            @include('layouts.partials.sidebars.admin-sidebar')
-                        </aside>
-                        <div class="col-md-9 content">
-                            @yield('content')
-                        </div>
+                <div class="row h-100">
+                    <aside class="col-md-3 py-3 border rounded-3">
+                        @include('layouts.partials.sidebars.app-sidebar')
+                    </aside>
+                    <div class="col-md-9 content">
+                        @yield('content')
                     </div>
-                @else
-                    <div class="px-4 py-5 my-5 text-center">
-                        <h1 class="display-5 fw-bold text-body-emphasis">
-                            Authorization
-                        </h1>
-                        <div class="col-lg-6 mx-auto">
-                            <p class="lead mb-4">
-                                You need to
-                                <a href="{{ route('register') }}">register an account</a>
-                                or if you already have one try to
-                                <a href="{{ route('login') }}">login</a>
-                            </p>
-                        </div>
+                </div>
+            @else
+                <div class="px-4 py-5 my-5 text-center">
+                    <h1 class="display-5 fw-bold text-body-emphasis">
+                        Authorization
+                    </h1>
+                    <div class="col-lg-6 mx-auto">
+                        <p class="lead mb-4">
+                            You need to
+                            <a href="{{ route('register') }}">register an account</a>
+                            or if you already have one try to
+                            <a href="{{ route('login') }}">login</a>
+                        </p>
                     </div>
-                @endauth
-            </div>
-        </main>
+                </div>
+            @endauth
+        </div>
+    </main>
 
-        @include('layouts.partials.footer')
-    </div>
+    @include('layouts.partials.footer')
+</div>
 </body>
 </html>
