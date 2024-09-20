29d8e4f988e2c2fec09f47317aa12a92773f804e
Ayr2017 <ayrat.2013@ya.ru>
2024-09-20 18:38:51 +0300

    main: feat (bolus reading)

--- app/Models/BolusReading.php
+++ app/Models/BolusReading.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class BolusReading extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'date',
+        'device_number',
+        'RSSI',
+        'AmM',
+        'CmM',
+        'AdX',
+        'AdY',
+        'AdZ',
+        'CdX',
+        'CdY',
+        'CdZ',
+        'PH',
+        'PN',
+        'UT',
+    ];
 }
