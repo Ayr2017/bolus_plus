351cd643c2eb94ad286f2a29a304356c98d74271
Ayr2017 <ayrat.2013@ya.ru>
2024-09-23 18:29:49 +0300

    main: feat (dashboards)

--- resources/views/dashboards/partials/create-dashboard-form.blade.php
+++ resources/views/dashboards/partials/create-dashboard-form.blade.php
@@ -2,7 +2,19 @@
     @csrf
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name"  name="name" aria-describedby="name">
+        <input type="text" class="form-control" id="name"  name="name" aria-describedby="name" value="{{old('name')}}">
+    </div>
+    <div class="mb-3">
+        <label for="icon" class="form-label">Icon</label>
+        <input type="text" class="form-control" id="icon"  name="icon" aria-describedby="icon" value="{{old('icon')}}">
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="form-control-sm form-control" id="description"  name="description" aria-describedby="description">{{old('description')}}</textarea>
+    </div>
+    <div class="mb-3">
+        <label for="icon" class="form-label">Picture</label>
+        <input type="file" class="form-control" id="picture"  name="picture" aria-describedby="picture" value="{{old('picture')}}">
     </div>
     <div class="my-1 border-top pt-2 border-2">
         <a href="{{route('events.index')}}" class="btn btn-sm btn-outline-secondary">Cancel</a>
