2dd1f02ce0e41185d101931b44d85de5330d6608
Ayr2017 <ayrat.2013@ya.ru>
2024-09-23 19:06:01 +0300

    main: feat (dashboards:show)

--- resources/views/dashboards/show.blade.php
+++ resources/views/dashboards/show.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="pb-2 mb-3 border-bottom d-flex align-items-center justify-content-between">
+        <h2>{{$title}}</h2>
+        <a class="btn btn-outline-secondary" href="{{route('dashboards.index')}}">Dashboards</a>
+    </div>
+
+    @dump($dashboard)
+
+@endsection
