1acdcd1ca31f8e632aa9a21e03da6ad9cd506497
Ayr2017 <ayrat.2013@ya.ru>
2024-10-01 17:44:09 +0300

    data-collector: review

--- datacollector/README.md
+++
@@ -1,3 +0,0 @@
-### 2024-01-24
-1. Собраны Dockerfile, docker-compose.yml, main.go
-2. Загружен golang Echo
--- datacollector/app/entities/bolus_reading.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type BolusReading struct {
-	ID             uint
-	Date           string
-	DeliveryNumber string
-	Rssi           float64
-	Ut             float64
-}
