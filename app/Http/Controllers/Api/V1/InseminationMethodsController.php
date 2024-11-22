1e2fcc17b6a0479028d7e028c39f0bd993a4c732
Ayr2017 <ayrat.2013@ya.ru>
2024-11-22 13:20:14 +0300

    BOL-25: insemination methods

--- app/Http/Controllers/Api/V1/InseminationMethodsController.php
+++ app/Http/Controllers/Api/V1/InseminationMethodsController.php
@@ -4,59 +4,107 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Helpers\ErrorLog;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\InseminationMethod\DeleteInseminationMethodRequest;
 use App\Http\Requests\InseminationMethod\IndexInseminationMethodRequest;
-use App\Http\Resources\Breed\BreedResource;
+use App\Http\Requests\InseminationMethod\ShowInseminationMethodRequest;
+use App\Http\Requests\InseminationMethod\StoreInseminationMethodRequest;
+use App\Http\Requests\InseminationMethod\UpdateInseminationMethodRequest;
+use App\Http\Resources\InseminationMethod\InseminationMethodResource;
 use App\Http\Responses\ApiResponse;
-use App\Services\InseminationMethods\InseminationMethodService;
+use App\Models\InseminationMethod;
+use App\Services\InseminationMethod\InseminationMethodService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class InseminationMethodsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @param IndexInseminationMethodRequest $request
+     * @param InseminationMethodService $inseminationMethodService
+     * @return JsonResponse
      */
-    public function index(IndexInseminationMethodRequest $request, InseminationMethodService $service): JsonResponse
+    public function index(IndexInseminationMethodRequest $request, InseminationMethodService $inseminationMethodService): JsonResponse
     {
         try {
-            $im =  $service->getInseminationMethods();
-            return ApiResponse::success(['items'=>$im]);
+            $inseminationMethods = $inseminationMethodService->getInseminationMethods($request->validated());
+            return ApiResponse::success(InseminationMethodResource::paginatedCollection($inseminationMethods));
         }catch (\Throwable $throwable){
-            ErrorLog::write(__METHOD__,__LINE__, $throwable->getMessage());
+            ErrorLog::write(__METHOD__,__LINE__,$throwable->getMessage());
+        }
+
+        return ApiResponse::error('Something went wrong!');
+    }
+
+
+    /**
+     * @param StoreInseminationMethodRequest $request
+     * @param InseminationMethodService $inseminationMethodService
+     * @return JsonResponse
+     */
+    public function store(StoreInseminationMethodRequest $request, InseminationMethodService $inseminationMethodService): JsonResponse
+    {
+        try {
+            $inseminationMethod = $inseminationMethodService->storeInseminationMethod($request->validated());
+            return ApiResponse::success(new InseminationMethodResource($inseminationMethod));
+        } catch (\Throwable $throwable){
+            ErrorLog::write(__METHOD__,__LINE__,$throwable->getMessage());
         }
 
         return ApiResponse::error('Something went wrong!');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @param ShowInseminationMethodRequest $request
+     * @param InseminationMethodService $service
+     * @param InseminationMethod $inseminationMethod
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function show(ShowInseminationMethodRequest $request, InseminationMethodService $service, InseminationMethod $inseminationMethod): JsonResponse
     {
-        //
+        try {
+            $inseminationMethod = $service->show($inseminationMethod);
+            return ApiResponse::success(['inseminationMethod' => InseminationMethodResource::make($inseminationMethod)]);
+        } catch (\Throwable $throwable) {
+            ErrorLog::write(__METHOD__, __LINE__, $throwable->getMessage());
+        }
+
+        return ApiResponse::error('Something went wrong!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
+     * @param UpdateInseminationMethodRequest $request
+     * @param InseminationMethodService $inseminationMethodService
+     * @param InseminationMethod $inseminationMethod
+     * @return JsonResponse
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateInseminationMethodRequest $request, InseminationMethodService $inseminationMethodService, InseminationMethod $inseminationMethod): JsonResponse
     {
-        //
+        try {
+            $inseminationMethod = $inseminationMethodService->updateInseminationMethod($request->validated(), $inseminationMethod);
+            return ApiResponse::success(['inseminationMethod' => InseminationMethodResource::make($inseminationMethod)]);
+        } catch (\Throwable $throwable) {
+            ErrorLog::write(__METHOD__, __LINE__, $throwable->getMessage());
+        }
+
+        return ApiResponse::error('Something went wrong!');
     }
 
     /**
      * Remove the specified resource from storage.
+     * @param DeleteInseminationMethodRequest $request
+     * @param InseminationMethodService $inseminationMethodService
+     * @param InseminationMethod $inseminationMethod
+     * @return JsonResponse
      */
-    public function destroy(string $id)
+    public function destroy(DeleteInseminationMethodRequest $request, InseminationMethodService $inseminationMethodService,  InseminationMethod $inseminationMethod): JsonResponse
     {
-        //
+        try {
+            return ApiResponse::success($inseminationMethodService->deleteInseminationMethod($inseminationMethod));
+        } catch (\Throwable $throwable) {
+            ErrorLog::write(__METHOD__, __LINE__, $throwable->getMessage());
+        }
+
+        return ApiResponse::error('Something went wrong!');
     }
 }
