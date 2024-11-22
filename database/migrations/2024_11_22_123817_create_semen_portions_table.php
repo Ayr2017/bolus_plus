0c3e795e23377cc6397c9720783b2bce7ba2c253
Ayr2017 <ayrat.2013@ya.ru>
2024-11-22 15:55:36 +0300

    BOL-25

--- database/migrations/2024_11_22_123817_create_semen_portions_table.php
+++ database/migrations/2024_11_22_123817_create_semen_portions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('semen_portions', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/SemenPortionSeeder.php
+++ database/seeders/SemenPortionSeeder.php
@@ -4,14 +4,34 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class SemenPortionSeeder extends Seeder
 {
+    const SEMEN_PORTIONS = [
+        ['id' => 1, 'name' => 'Полная'],
+        ['id' => 2, 'name' => 'Половина'],
+        ['id' => 3, 'name' => 'Четверть'],
+        ['id' => 4, 'name' => 'Двойная'],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $timestamp = Carbon::now();
+
+        // Добавляем временные метки ко всем элементам массива
+        $dataWithTimestamps = array_map(function ($item) use ($timestamp) {
+            return array_merge($item, [
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ]);
+        }, self::SEMEN_PORTIONS);
+
+        // Вставка данных с временными метками
+        DB::table('semen_portions')->insertOrIgnore($dataWithTimestamps);
     }
 }
