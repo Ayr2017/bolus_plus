9579f7ced6110d2f47fc5149fad4829ec8020931
Ayr2017 <ayrat.2013@ya.ru>
2024-12-06 23:06:10 +0300

    Add CountriesEnum to optimize country management
    Introduced the CountriesEnum to provide a structured approach for managing country identifiers within the application. This update enhances maintainability and reduces the risk of errors associated with hardcoded country strings. The use of enums ensures clear and consistent use of country identifiers across the codebase.

---
+++ app/Enums/Country/CountriesEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums\Country;
+
+enum CountriesEnum: string {
+    case NETHERLANDS = 'NETHERLANDS';
+    case ITALY = 'ITALY';
+    case GERMANY = 'GERMANY';
+    case CANADA = 'CANADA';
+    case ESTONIA = 'ESTONIA';
+    case USA = 'USA';
+    case RUSSIA = 'RUSSIA';
+    case CZECH_REPUBLIC = 'CZECH_REPUBLIC';
+    case BELGIUM = 'BELGIUM';
+    case FINLAND = 'FINLAND';
+    case DENMARK = 'DENMARK';
+    case VIRGIN_ISLANDS_USA = 'VIRGIN_ISLANDS_USA';
+}
