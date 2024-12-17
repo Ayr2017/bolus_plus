35af580f0b94c757046ec5e514e9c78a5d0a4b79
averines <averines@mail.ru>
2024-12-18 01:11:22 +0300

    tests: breeding-bull (model, api)

---
+++ tests/Feature/Controllers/Api/BreedingBullsControllerTest.php
@@ -0,0 +1,398 @@
+<?php
+
+namespace Feature\Controllers\Api;
+
+use AllowDynamicProperties;
+use App\Models\BreedingBull;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+#[AllowDynamicProperties] class BreedingBullsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+
+        // TODO: заполнить сидер
+        $this->artisan('db:seed --class=BreedingBullSeeder');
+
+
+        BreedingBull::create(['type' => 'type1']);
+        BreedingBull::create(['type' => 'type2']);
+    }
+
+    public function test_index_for_admin()
+    {
+        $response = $this->actingAs($this->admin)->getJson(route('api.breeding-bulls.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'items',
+                "current_page",
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_index_for_non_admin()
+    {
+        $response = $this->actingAs($this->user)->getJson(route('api.breeding-bulls.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'items',
+                "current_page",
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_store_for_admin()
+    {
+        $data = [
+            'type' => 'Test Type',
+            'rshn_id' => 'Test RSHN ID',
+        ];
+
+        $response = $this->actingAs($this->admin)->postJson(route('api.breeding-bulls.store'), $data);
+
+        // TODO: привести к единому виду:
+        // в api response           seed_supplier и seed_code
+        // в модели                 seed_supplier и semen_code
+        // в миграции               semen_supplier и semen_code
+        $this->assertTrue(False); // это специальный фэйл для привлечения внимания, удалить после исправления
+
+
+        // TODO: после создания отдавать 201 вместо 200
+        $response->assertCreated();
+
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'type',
+                'seed_supplier',
+                'nickname',
+                'date_of_birth',
+                'country_of_birth',
+                'tag_number',
+                'seed_code',
+                'rshn_id',
+                'color',
+                'is_selected',
+                'is_active',
+                'is_own',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        $this->assertDatabaseHas('breeding_bulls', $data);
+    }
+
+    public function test_store_for_non_admin()
+    {
+        $data = [
+            'type' => 'Test Type',
+            'rshn_id' => 'Test RSHN ID',
+        ];
+
+        $response = $this->actingAs($this->user)->postJson(route('api.breeding-bulls.store'), $data);
+
+        // TODO: после создания отдавать 201 вместо 200
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'type',
+                'seed_supplier',
+                'nickname',
+                'date_of_birth',
+                'country_of_birth',
+                'tag_number',
+                'seed_code',
+                'rshn_id',
+                'color',
+                'is_selected',
+                'is_active',
+                'is_own',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        $this->assertDatabaseHas('breeding_bulls', $data);
+    }
+
+    public function test_show_for_admin()
+    {
+        $breedingBull = BreedingBull::query()->first();
+        $response = $this->actingAs($this->admin)->json('GET', route('api.breeding-bulls.show', $breedingBull), ['breeding_bull' => $breedingBull->id]);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'type',
+                'seed_supplier',
+                'nickname',
+                'date_of_birth',
+                'country_of_birth',
+                'tag_number',
+                'seed_code',
+                'rshn_id',
+                'color',
+                'is_selected',
+                'is_active',
+                'is_own',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                'id' => $breedingBull->id,
+                'type' => $breedingBull->type,
+                'seed_supplier' => $breedingBull->seed_supplier,
+                'nickname' => $breedingBull->nickname,
+                'date_of_birth' => $breedingBull->date_of_birth,
+                'country_of_birth' => $breedingBull->country_of_birth,
+                'tag_number' => $breedingBull->tag_number,
+                'seed_code' => $breedingBull->seed_code,
+                'rshn_id' => $breedingBull->rshn_id,
+                'color' => $breedingBull->color,
+                'is_selected' => $breedingBull->is_selected,
+                'is_active' => $breedingBull->is_active,
+                'is_own' => $breedingBull->is_own,
+                'created_at' => $breedingBull->created_at,
+                'updated_at' => $breedingBull->updated_at,
+            ]
+        ]);
+    }
+
+    public function test_show_for_non_admin()
+    {
+        $breedingBull = BreedingBull::query()->first();
+        $response = $this->actingAs($this->user)->json('GET', route('api.breeding-bulls.show', $breedingBull), ['breeding_bull' => $breedingBull->id]);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'type',
+                'seed_supplier',
+                'nickname',
+                'date_of_birth',
+                'country_of_birth',
+                'tag_number',
+                'seed_code',
+                'rshn_id',
+                'color',
+                'is_selected',
+                'is_active',
+                'is_own',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                'id' => $breedingBull->id,
+                'type' => $breedingBull->type,
+                'seed_supplier' => $breedingBull->seed_supplier,
+                'nickname' => $breedingBull->nickname,
+                'date_of_birth' => $breedingBull->date_of_birth,
+                'country_of_birth' => $breedingBull->country_of_birth,
+                'tag_number' => $breedingBull->tag_number,
+                'seed_code' => $breedingBull->seed_code,
+                'rshn_id' => $breedingBull->rshn_id,
+                'color' => $breedingBull->color,
+                'is_selected' => $breedingBull->is_selected,
+                'is_active' => $breedingBull->is_active,
+                'is_own' => $breedingBull->is_own,
+                'created_at' => $breedingBull->created_at,
+                'updated_at' => $breedingBull->updated_at,
+            ]
+        ]);
+    }
+
+    public function test_update_for_admin()
+    {
+        $breedingBull = BreedingBull::query()->first();
+
+        $data = [
+            'type' => 'Updated Type',
+            'seed_supplier' => 'Updated Seed Supplier',
+            'nickname' => 'Updated Nickname',
+            'date_of_birth' =>  now(),
+            'country_of_birth' => 'Updated Country of Birth',
+            'tag_number' => 'Updated Tag Number',
+            'semen_code' => 'Updated Semen Code',
+            'rshn_id' => 'Updated RSHN ID',
+            'coat_color_id' => 2,
+            'breed_id' => 2,
+            'is_selected' => false,
+            'is_active' => false,
+            'is_own' => false,
+        ];
+
+        // TODO: почему-то ищет колонку 'created_at:"2024-12-17T22:02:26.000000Z"' прямо целиком. ошибка синтаксиса.
+        $response = $this->actingAs($this->admin)->putJson(route('api.breeding-bulls.update', $breedingBull), $data);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'type',
+                'seed_supplier',
+                'nickname',
+                'date_of_birth',
+                'country_of_birth',
+                'tag_number',
+                'seed_code',
+                'rshn_id',
+                'color',
+                'is_selected',
+                'is_active',
+                'is_own',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        $this->assertDatabaseHas('breeding_bulls', $data);
+    }
+
+    public function test_update_for_non_admin()
+    {
+        $breedingBull = BreedingBull::query()->first();
+
+        $data = [
+            'type' => 'Updated Type',
+            'seed_supplier' => 'Updated Seed Supplier',
+            'nickname' => 'Updated Nickname',
+            'date_of_birth' =>  now(),
+            'country_of_birth' => 'Updated Country of Birth',
+            'tag_number' => 'Updated Tag Number',
+            'semen_code' => 'Updated Semen Code',
+            'rshn_id' => 'Updated RSHN ID',
+            'coat_color_id' => 2,
+            'breed_id' => 2,
+            'is_selected' => false,
+            'is_active' => false,
+            'is_own' => false,
+        ];
+
+        // TODO: почему-то ищет колонку 'created_at:"2024-12-17T22:02:26.000000Z"' прямо целиком. ошибка синтаксиса.
+        $response = $this->actingAs($this->admin)->putJson(route('api.breeding-bulls.update', $breedingBull), $data);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'type',
+                'seed_supplier',
+                'nickname',
+                'date_of_birth',
+                'country_of_birth',
+                'tag_number',
+                'seed_code',
+                'rshn_id',
+                'color',
+                'is_selected',
+                'is_active',
+                'is_own',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        $this->assertDatabaseHas('breeding_bulls', $data);
+    }
+
+    public function test_destroy_for_admin()
+    {
+        $breedingBull = BreedingBull::query()->first();
+
+        $response = $this->actingAs($this->admin)->deleteJson(route('api.breeding-bulls.destroy', $breedingBull), ['breeding_bull' => $breedingBull->id]);
+
+        $response->assertOk();
+        $response->assertJson([
+            'success' => true,
+            'error' => null,
+        ]);
+
+        $this->assertDatabaseMissing('breeding_bulls', ['id' => $breedingBull->id]);
+    }
+
+    public function test_destroy_for_non_admin()
+    {
+        $breedingBull = BreedingBull::query()->first();
+
+        $response = $this->actingAs($this->user)->deleteJson(route('api.breeding-bulls.destroy', $breedingBull), ['breeding_bull' => $breedingBull->id]);
+
+        $response->assertOk();
+        $response->assertJson([
+            'success' => true,
+            'error' => null,
+        ]);
+
+        $this->assertDatabaseMissing('breeding_bulls', ['id' => $breedingBull->id]);
+    }
+
+    // TODO: пользователи могут удалять?
+    // public function test_destroy_forbidden_for_non_admin()
+    // {
+    //     $breedingBull = BreedingBull::query()->first();
+
+    //     $response = $this->actingAs($this->user)->deleteJson(route('api.breeding-bulls.destroy', $breedingBull), ['breeding_bull' => $breedingBull->id]);
+
+    //     $response->assertForbidden();
+
+    //     $this->assertDatabaseHas('breeding_bulls', ['id' => $breedingBull->id]);
+    // }
+}
---
+++ tests/Models/BreedingBullTest.php
@@ -0,0 +1,320 @@
+<?php
+
+namespace Feature\Models;
+
+use AllowDynamicProperties;
+use App\Models\BreedingBull;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Database\QueryException;
+use Carbon\Carbon;
+
+
+use function Symfony\Component\Clock\now;
+
+#[AllowDynamicProperties] class BreedingBullTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_mass_assignment()
+    {
+        $data = [
+            'type' => 'Test Type',
+            // TODO: в модели в fillable seed_supplier переименовать в semen_supplier, как указано в миграции. и тут тоже
+            'seed_supplier' => 'Test Seed Supplier',
+            'nickname' => 'Test Nickname',
+            'date_of_birth' =>  now(),
+            'country_of_birth' => 'Test Country of Birth',
+            'tag_number' => 'Test Tag Number',
+            'semen_code' => 'Test Semen Code',
+            'rshn_id' => 'Test RSHN ID',
+            'coat_color_id' => 1,
+            'breed_id' => 1,
+            'is_selected' => true,
+            'is_active' => true,
+            'is_own' => true,
+        ];
+
+        $result = BreedingBull::create($data);
+
+        foreach ($data as $key => $value) {
+            $this->assertEquals($value, $result->$key);
+        }
+    }
+
+    public function test_create()
+    {
+        $data = [
+            'type' => 'Test Type',
+        ];
+
+        BreedingBull::create($data);
+
+        $this->assertDatabaseHas('breeding_bulls', $data);
+    }
+
+    public function test_read()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+        ]);
+
+        $found = BreedingBull::find($result->id);
+        $this->assertEquals('Test Type', $found->type);
+    }
+
+    public function test_update()
+    {
+        $today = Carbon::now();
+        $tomorrow = $today->addDay();
+
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            // TODO: в модели в fillable seed_supplier переименовать в semen_supplier, как указано в миграции. и тут тоже
+            'seed_supplier' => 'Test Seed Supplier',
+            'nickname' => 'Test Nickname',
+            'date_of_birth' => $today,
+            'country_of_birth' => 'Test Country of Birth',
+            'tag_number' => 'Test Tag Number',
+            'semen_code' => 'Test Semen Code',
+            'rshn_id' => 'Test RSHN ID',
+            'coat_color_id' => 1,
+            'breed_id' => 1,
+            'is_selected' => true,
+            'is_active' => true,
+            'is_own' => true,
+        ]);
+
+        $result->update([
+            'type' => 'Updated Type',
+            // TODO: в модели в fillable seed_supplier переименовать в semen_supplier, как указано в миграции. и тут тоже
+            'seed_supplier' => 'Updated Seed Supplier',
+            'nickname' => 'Updated Nickname',
+            'date_of_birth' =>  $tomorrow,
+            'country_of_birth' => 'Updated Country of Birth',
+            'tag_number' => 'Updated Tag Number',
+            'semen_code' => 'Updated Semen Code',
+            'rshn_id' => 'Updated RSHN ID',
+            'coat_color_id' => 2,
+            'breed_id' => 2,
+            'is_selected' => false,
+            'is_active' => false,
+            'is_own' => false,
+        ]);
+
+        $this->assertDatabaseHas('breeding_bulls', [
+            'type' => 'Updated Type',
+            // TODO: в модели в fillable seed_supplier переименовать в semen_supplier, как указано в миграции. и тут тоже
+            'seed_supplier' => 'Updated Seed Supplier',
+            'nickname' => 'Updated Nickname',
+            'date_of_birth' =>  $tomorrow,
+            'country_of_birth' => 'Updated Country of Birth',
+            'tag_number' => 'Updated Tag Number',
+            'semen_code' => 'Updated Semen Code',
+            'rshn_id' => 'Updated RSHN ID',
+            'coat_color_id' => 2,
+            'breed_id' => 2,
+            'is_selected' => false,
+            'is_active' => false,
+            'is_own' => false,
+        ]);
+    }
+
+    public function test_delete()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+        ]);
+
+        $result->delete();
+
+        $this->assertDatabaseMissing('breeding_bulls', [
+            'id' => $result->id,
+        ]);
+    }
+
+    public function test_type_is_required()
+    {
+        $this->expectException(QueryException::class);
+        BreedingBull::create(['type' => null]);
+    }
+
+    public function test_rshn_id_is_required()
+    {
+        // ошибка не возникает, так как rshn_id nullable (это возможно ошибочно)
+        // $this->expectException(QueryException::class);
+        // BreedingBull::create(['rshn_id' => null]);
+
+        // TODO: в миграции rshn_id nullable, но в StoreBreedingBullRequest rshn_id required - привести к единому соглашению
+        $result = BreedingBull::create(['type' => 'Test Type', 'rshn_id' => null]);
+        $this->assertNotNull($result->rshn_id);
+    }
+
+    public function test_semen_supplier_is_nullable()
+    {
+        // TODO: seed_supplier переименовать в semen_supplier, как указано в миграции. и тут тоже
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'seed_supplier' => null,
+        ]);
+
+        $this->assertNull($result->seed_supplier);
+    }
+
+    public function test_nickname_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'nickname' => null,
+        ]);
+
+        $this->assertNull($result->nickname);
+    }
+
+    public function test_date_of_birth_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'date_of_birth' => null,
+        ]);
+
+        $this->assertNull($result->date_of_birth);
+    }
+
+    public function test_country_of_birth_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'country_of_birth' => null,
+        ]);
+
+        $this->assertNull($result->country_of_birth);
+    }
+
+    public function test_tag_number_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'tag_number' => null,
+        ]);
+
+        $this->assertNull($result->tag_number);
+    }
+
+    public function test_semen_code_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'semen_code' => null,
+        ]);
+
+        $this->assertNull($result->semen_code);
+    }
+
+    public function test_rshn_id_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'rshn_id' => null,
+        ]);
+
+        $this->assertNull($result->rshn_id);
+    }
+
+    public function test_coat_color_id_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'coat_color_id' => null,
+        ]);
+
+        $this->assertNull($result->coat_color_id);
+    }
+
+    public function test_breed_id_is_nullable()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'breed_id' => null,
+        ]);
+
+        $this->assertNull($result->breed_id);
+    }
+
+    public function test_is_selected_default_is_false()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+        ]);
+
+        $result->refresh();
+
+        // TODO: в модели кастовать is_selected to boolean
+        $this->assertFalse($result->is_selected);
+    }
+
+    public function test_is_active_default_is_true()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+        ]);
+
+        $result->refresh();
+
+        $this->assertTrue($result->is_active);
+    }
+
+    public function test_is_own_default_is_false()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+        ]);
+
+        $result->refresh();
+
+        $this->assertFalse($result->is_own);
+    }
+
+    public function test_is_selected_casts_to_boolean()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'is_selected' => 1
+        ]);
+
+        // TODO: в модели кастовать is_selected to boolean
+        $this->assertTrue($result->is_selected);
+    }
+
+    public function test_is_active_casts_to_boolean()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'is_active' => 1
+        ]);
+
+        $this->assertTrue($result->is_active);
+    }
+
+    public function test_is_own_casts_to_boolean()
+    {
+        $result = BreedingBull::create([
+            'type' => 'Test Type',
+            'is_own' => 1
+        ]);
+
+        $this->assertTrue($result->is_own);
+    }
+
+    public function test_factory()
+    {
+        $result = BreedingBull::factory()->create();
+
+        $this->assertDatabaseHas('breeding_bulls', [
+            'id' => $result->id,
+        ]);
+
+        // TODO: убрать HasFactory из модели или настроить фабрику (файл фабрики есть, нужно указать обязательные поля)
+        $this->assertNotNull($result->name);
+    }
+}
